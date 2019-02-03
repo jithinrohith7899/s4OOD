7c7e983c7d65cc92c43ebabe795410fc02b2ec13
jithinrohith7899 <47301793+jithinrohith7899@users.noreply.github.com>
2019-02-03 23:03:27 +0530

    38. Jithin Rohith KR
    pattern

---
+++ jithinrohith_38.java
@@ -0,0 +1,45 @@
+public class jithinrohith_38 {
+public static void main(String[] args) {
+    System.out.println(" 		    1");                   
+    System.out.println("                   111");                  
+    System.out.println("                  11111");                 
+    System.out.println("            	 1111111");                
+    System.out.println("                111111111");               
+    System.out.println("               11111111111");              
+    System.out.println("              1111111111111");             
+    System.out.println("             111111111111111");            
+    System.out.println("            11111111111111111");           
+    System.out.println("           1111111111111111111");          
+    System.out.println("          111111111111111111111");         
+    System.out.println("         11111111111 11111111111");       
+    System.out.println("        11111111111   11111111111");       
+    System.out.println("       11111111111     11111111111");      
+    System.out.println("      11111111111       11111111111");     
+    System.out.println("     11111111111         11111111111");    
+    System.out.println("    11111111111           11111111111");   
+    System.out.println("   11111111111             11111111111");  
+    System.out.println("  11111111111               11111111111");
+    System.out.println(" 11111111111                 11111111111");
+    System.out.println("11111111111                   11111111111");
+    System.out.println(" 11111111111                 11111111111"); 
+    System.out.println("  11111111111               11111111111");  
+    System.out.println("   11111111111             11111111111");    
+    System.out.println("    11111111111           11111111111");     
+    System.out.println("     11111111111         11111111111");      
+    System.out.println("      11111111111       11111111111");      
+    System.out.println("       11111111111     11111111111");        
+    System.out.println("        11111111111   11111111111");         
+    System.out.println("         11111111111 11111111111");          
+    System.out.println("          111111111111111111111");           
+    System.out.println("           1111111111111111111");            
+    System.out.println("            11111111111111111");             
+    System.out.println("             111111111111111");              
+    System.out.println("              1111111111111");               
+    System.out.println("               11111111111");                
+    System.out.println("                111111111");                 
+    System.out.println("                 1111111");                  
+    System.out.println("                  11111");  
+    System.out.println("                   111");
+    System.out.println("                    1");
+	}
+}
